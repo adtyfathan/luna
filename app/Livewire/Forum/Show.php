d8a97b7192a663fa9d7d110c65f65598ec011a10
adtyfathan <adtyfathan04@gmail.com>
2025-08-08 21:05:51 +0700

    make forum view

---
+++ app/Livewire/Forum/Show.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Forum;
+
+use Livewire\Component;
+
+class Show extends Component
+{
+    public function render()
+    {
+        return view('livewire.forum.show');
+    }
+}
